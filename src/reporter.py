074f3c62d75a6dbe282c48c6c1a42f8fe9b974b8
Yury Todorov <yuri.todorov@yandex.ru>
2026-09-06 13:46:02 +0300

    Added  def show_report and read_csv

--- src/reporter.py
+++ src/reporter.py
@@ -3,3 +3,28 @@ class DataFrameReporter:
         self.float_format = float_format
         self.percent_format = percent_format
         self.include_all = include_all
+
+    def show_report(self, df, title=None):
+        if title:
+            print(title)
+    
+        print('Количество столбцов:', df.shape[1])
+        print('Количество строк:', df.shape[0])
+
+        duplicates = df.duplicated().sum()
+        print('Количество дубликатов:', duplicates)
+
+        print('Доля дубликатов:', format(duplicates / df.shape[0], self.percent_format))
+
+        print(df.describe(include='all' if self.include_all else None))
+        
+        # выведите количество пропусков во всем датафрейме одним числом
+        print('Количество пропусков:', df.isna().sum().sum())
+        
+        # выведите долю пропусков во всем датафрейме одним числом с плавающей точкой
+        # в формате float_format
+        print('Доля пропусков:', format(df.isna().mean().mean(), self.float_format))
+
+import pandas as pd
+
+data = pd.read_csv('data/payments.csv')
