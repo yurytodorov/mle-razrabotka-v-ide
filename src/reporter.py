e17290bc76b0d2f9e8fd22adc03b4e7b86710863
Yury Todorov <yuri.todorov@yandex.ru>
2026-09-06 13:40:43 +0300

    Added payments.csv and class DataFrameReporter

---
+++ src/reporter.py
@@ -0,0 +1,5 @@
+class DataFrameReporter:
+    def __init__(self, float_format='0.05f', percent_format='0.02%', include_all=False):
+        self.float_format = float_format
+        self.percent_format = percent_format
+        self.include_all = include_all
